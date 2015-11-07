340963c03586c60aaf61b2f06d698c6a6c743af6
Sophie Qiu <qiuhuilian1991@gmail.com>
2015-11-07 12:02:29 -0500

    works, no mem error

---
+++ xprintf.h
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+#ifndef XPRINTF
+#define XPRINTF
+
+typedef struct queue * queue_t;
+
+/*Initializes a queue with space enough for message_num. Returns the pointer to the structure.*/
+queue_t xprintf_init(int message_num);
+
+/*Adds format and corresponding va_list to the queue.*/
+void xprintf(queue_t, char*, ...);
+
+/*Prints out all messages stored in queue in FIFO order to stream.*/
+void xprintf_fini(queue_t, FILE* stream);
+
+void xprintf_free(queue_t);
+
+int get_length(queue_t);
+
+#endif
